400ca597b376bf402794b2139234d311b83ae86e
dubkid2 <dub_kid2@live.com>
2021-07-29 01:20:16 -0500

    Title screen
    Added a title screen. Everything is being done with JFrame for now. 
    Added:
    Buttons with listeners
    Textfield with listener
    Text interpreter for field

--- Wasteland Weirdos/src/Screen.java
+++ Wasteland Weirdos/src/Screen.java
@@ -1,19 +1,28 @@
 
 
 import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.plaf.TextUI;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
 
-public class Screen {
-	
+
+public class Screen extends JFrame implements ActionListener {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JFrame Window;
 	private GraphicsDevice vc;
-	private int height=10;
-	private int width=10;
+	private JTextField field;
+	private JPanel window;
+	private JPanel BR;
+
 	
 	public Screen() {
 		
@@ -33,66 +42,141 @@ public class Screen {
 		 
 		window.setVisible(true);	 
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Title(window);
+		
 
 		
 	}
 	
-	public void SetWindowed(DisplayMode dm, JFrame window) {
+	public void SetWindowed(DisplayMode dm, JFrame windows) {
+		Window=windows;
+		
+		window= new JPanel();
+		window.setLayout(null);
+		BR = new JPanel();
+		BR.setLayout(null);
+		BR.setSize(800, 800);
 		DisplayMode windowed= vc.getDisplayMode();
-		vc.setFullScreenWindow(window);
-		
+		vc.setFullScreenWindow(windows);
 		vc.setDisplayMode(windowed);
-		Color BR = new Color(170, 229, 47);
-		 window.getContentPane().setBackground(BR);
+		window.setSize(800,800);
+		windows.setSize(800, 800);
+		Color BRC = new Color(170, 229, 47);
+		 window.setBackground(BRC);
 		//window.setUndecorated(false);
-		window.setResizable(true);		
+		windows.setResizable(false);		
 		window.setVisible(true);	 
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		Title(window);
-	       
-	   
-	}
-	
-	private void Title(JFrame window) {
-		int HsSize=400;
+		windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windows.add(window);
+        windows.add(BR);
+         
 
-		window.setTitle("Wasteland Weirdos");
-		 height= window.getHeight();
-			width = window.getWidth();
-			 System.out.println(height);
-			 
-			 JLabel label1 = new JLabel("Wasteland Weirdos");
-			
-			 label1.setBounds(width/2-200, 0, 1000,500);
+		int HsSize=400;
+		
+		windows.setTitle("Wasteland Weirdos");
+		
+		JLabel label1= new JLabel("Wasteland Weirdos");
+			 label1.setBounds(50,100, 800,75);
 			 label1.setVisible(true);
-			 label1.setFont(new Font("Comic Sans MS", Font.PLAIN, 50));
+			 label1.setFont(new Font("Comic Sans MS", Font.PLAIN, 75));
 			 window.add(label1);
 			 
 			 
 			 
 			 ImageIcon image = new ImageIcon("src/hs.jpg");
 	        JLabel imageLabel = new JLabel(image); 
-	        window.add(imageLabel);
-	        imageLabel.setBounds(width/2-HsSize/2, height/2-HsSize/2, HsSize, HsSize);
+	        imageLabel.setBounds(200, 200, HsSize, HsSize);
 	        imageLabel.setVisible(true);
+	        window.add(imageLabel);
+
+//	        ImageIcon BR = new ImageIcon("src/BR.jpg");
+//	        JLabel BRLabel = new JLabel(BR); 
+//	        window.add(BRLabel);
+//	       BRLabel.setBounds(0, 0, width, height);
+//	       BRLabel.setVisible(true);
 	        
-	        ImageIcon BR = new ImageIcon("src/BR.jpg");
-	        JLabel BRLabel = new JLabel(BR); 
-	        window.add(BRLabel);
-	       BRLabel.setBounds(0, 0, width, height);
-	       BRLabel.setVisible(true);
-	       
-	        JTextField field = new JTextField(10);
-	        window.add(field);
-	        field.setBounds(0,height-50, width, 50);
+	        JButton Start = new JButton ("New Game");
+	        Start.setVisible(true);
+	        Start.setBounds(200,650, 200, 50);
+	        Start.addActionListener(this);
+	        window.add(Start);
+
+	        
+	        JButton Settings = new JButton ("settings");
+	        Settings.setVisible(true);
+	        Settings.setBounds(400, 650, 200, 50);
+	        Settings.addActionListener(this);
+	        window.add(Settings);
+
+
+	        field= new JTextField(25);
+	        field.addActionListener(this);
+	        field.setActionCommand("Text");
+	        field.setBounds(50,700, 700, 50);
 	        field.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
 	        field.setVisible(true);
-	}
+	        BR.add(field);
+	        BR.repaint();
+	        window.repaint();
+
+	        SimpleAudioPlayer audioPlayer;
+			try {
+				audioPlayer = new SimpleAudioPlayer();
+			    audioPlayer.play();
+			} catch (UnsupportedAudioFileException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (LineUnavailableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+      
+		}
 	
+	
+
 	public DisplayMode GetDM() {
 		return vc.getDisplayMode();
 	}
+	
+	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String action=arg0.getActionCommand();
+		//System.out.println("something happened: " + action);
+		if(action.equals("New Game") ) {		System.out.println("The NewGame button has been pressed");}
+		
+		if(action.equals("settings") ) {		System.out.println("The other button has been pressed");}
+		
+		
+		if(action.equals("Text")     ) {		try {
+			String text = field.getDocument().getText(0, field.getDocument().getLength());
+			switch(text) {
+			case "bruh": 	        window.removeAll(); window.repaint();
+				break;
+				
+			case "yeet": Window.dispatchEvent(new WindowEvent(Window, WindowEvent.WINDOW_CLOSING));
+				break;
+			default: System.out.println("Unrecognized command: " + text);
+			}
+			field.setText("");
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}}
+
+	}
+	
+	private void GoToScreen(int sc) {
+		window.removeAll();
+		
+		
+		
+	}
+	
+	
 
 }
--- Wasteland Weirdos/src/WastelandWeirdos.java
+++ Wasteland Weirdos/src/WastelandWeirdos.java
@@ -4,12 +4,12 @@ import javax.swing.JFrame;
 public class WastelandWeirdos {
 	
 	public static void main(String[] args) {
-		System.out.println("hello world");
 		
 		JFrame window = new JFrame();
 		Screen screen= new Screen();
-		screen.SetFullscreen(null, window);
-		//screen.SetWindowed(null, window);
+		//screen.SetFullscreen(null, window);
+		screen.SetWindowed(null, window);
+		
 		
 	}
 
